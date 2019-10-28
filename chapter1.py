c32c384b162e843382f6be89203c22e1279480f9
jpbraid <45018981+jpbraid@users.noreply.github.com>
2019-10-29 02:23:04 +1100

    Update chapter1.py

--- chapter1.py
+++ chapter1.py
@@ -11,11 +11,10 @@ print(token.text)
 
 f = open('data/yelp/v6/yelp_academic_dataset_review.json')
 js = []
-
 for i in range(10000):
     js.append(json.loads(f.readline()))
-    
 f.close()
+
 review_df = pd.DataFrame(js)
 bow_converter = CountVectorizer(token_pattern='(?u)\\b\\w+\\b')
 
